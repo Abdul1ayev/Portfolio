740c247d05563b6f3aa7ea5b60d485902da1df29
Abdul1ayev <abdullayevsamir2009@gmail.com>
2025-03-24 11:51:12 +0500

    fixVercelErrors

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,7 @@
 "use client";
+import { createClient } from "@/supabase/client";
 import Image from "next/image";
-import { useEffect, useState } from "react";
-const { createClient } = require("@/supabase/client");
+import { useState } from "react";
 
 const supabase = createClient();
 type Project = {
